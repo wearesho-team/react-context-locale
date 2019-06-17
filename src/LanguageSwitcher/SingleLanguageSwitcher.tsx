d00ecca451ab33e7c647fcb4ee6ae635e20c8c24
Alexander Letnikow <reclamme@gmail.com>
2019-06-17 18:54:40 +0300

    Fix single language switcher click handler

--- src/LanguageSwitcher/SingleLanguageSwitcher.tsx
+++ src/LanguageSwitcher/SingleLanguageSwitcher.tsx
@@ -4,12 +4,12 @@ import { LocaleProviderContext } from "../LocaleProvider";
 
 export interface SingleLanguageSwitcherProps extends React.HTMLProps<HTMLButtonElement> {
     render?: (label: string, locale: string) => React.ReactNode;
-    localeLabels?: { [key: string]: string };
+    localeLabels?: { [ key: string ]: string };
 }
 
 const getNextLocale = (current: string, available: Array<string>): string => {
-    return available.find((_, i) => available[i - 1] === current)
-        || available[0];
+    return available.find((_, i) => available[ i - 1 ] === current)
+        || available[ 0 ];
 };
 
 export const SingleLanguageSwitcher: React.FC<SingleLanguageSwitcherProps> = (props) => {
@@ -19,7 +19,7 @@ export const SingleLanguageSwitcher: React.FC<SingleLanguageSwitcherProps> = (pr
     const handleClick = React.useCallback((e) => {
         onClick && onClick(e);
         context.setLocale(nextLocale);
-    }, [onClick, context.setLocale]);
+    }, [ onClick, context.setLocale, nextLocale ]);
 
     const label = props.localeLabels ? props.localeLabels[ nextLocale ] : nextLocale;
     return (
